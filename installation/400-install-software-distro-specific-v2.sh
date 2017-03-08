57f3bc2e96e8a1e5778bef2e52c1060a69094803
Erik Dubois <erik.dubois@gmail.com>
2017-03-08 12:07:16 +0100

    Comment :  on Wed Mar  8 12:07:16 CET 2017

--- installation/400-install-software-distro-specific-v2.sh
+++ installation/400-install-software-distro-specific-v2.sh
@@ -124,10 +124,6 @@ fi
 
 
 
-sudo pacman -R obkey --confirm
-
-
-
 
 
 
@@ -156,17 +152,17 @@ else
 	if pacman -Qi packer &> /dev/null; then
 
 		echo "Installing with packer"
-		packer -S --noconfirm --noedit  $package
+		packer -S  --noedit  $package
 
 	elif pacman -Qi pacaur &> /dev/null; then
 		
 		echo "Installing with pacaur"
-		pacaur -S --noconfirm --noedit  $package
+		pacaur -S  --noedit  $package
 		 	
 	elif pacman -Qi yaourt &> /dev/null; then
 
 		echo "Installing with yaourt"
-		yaourt -S --noconfirm $package
+		yaourt -S  $package
 			  	
 	fi
 
